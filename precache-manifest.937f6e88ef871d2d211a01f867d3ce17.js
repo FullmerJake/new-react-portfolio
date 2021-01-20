0c3433ad7d879e7872c80da3f90e137879058695
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-20 02:23:45 -0700

    Updates

---
+++ precache-manifest.937f6e88ef871d2d211a01f867d3ce17.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb5562a248a8bcec73d93efcb7263b2",
+    "url": "/new-react-portfolio/index.html"
+  },
+  {
+    "revision": "a2e9f2764e852cb9950f",
+    "url": "/new-react-portfolio/static/css/2.e5dec2c9.chunk.css"
+  },
+  {
+    "revision": "eab549bcfd488fadf966",
+    "url": "/new-react-portfolio/static/css/main.f9ba1bce.chunk.css"
+  },
+  {
+    "revision": "a2e9f2764e852cb9950f",
+    "url": "/new-react-portfolio/static/js/2.68cd0b91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/new-react-portfolio/static/js/2.68cd0b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab549bcfd488fadf966",
+    "url": "/new-react-portfolio/static/js/main.0455c323.chunk.js"
+  },
+  {
+    "revision": "311828b378504d83c07a",
+    "url": "/new-react-portfolio/static/js/runtime-main.f73d8cd3.js"
+  },
+  {
+    "revision": "e61d21a789681c792bac97115903a20a",
+    "url": "/new-react-portfolio/static/media/agilePeakImage.e61d21a7.png"
+  },
+  {
+    "revision": "1b7a7e61f0ee6406792728fbb483aa84",
+    "url": "/new-react-portfolio/static/media/bookSearchImage.1b7a7e61.png"
+  },
+  {
+    "revision": "a14d56451416cf76de5245df2eb4b5c5",
+    "url": "/new-react-portfolio/static/media/budgetTrackerImage.a14d5645.png"
+  },
+  {
+    "revision": "86ccb3ee2cbee2dc9598e9e8f059697e",
+    "url": "/new-react-portfolio/static/media/gitItDoneImage.86ccb3ee.png"
+  },
+  {
+    "revision": "3a5e058057e3180fe520deecb1c3c8ca",
+    "url": "/new-react-portfolio/static/media/jumpyManImage.3a5e0580.png"
+  },
+  {
+    "revision": "11e668b01605f3ab8d45721730d4a163",
+    "url": "/new-react-portfolio/static/media/noteTaker.11e668b0.png"
+  },
+  {
+    "revision": "a00bdf996801548fae927f112a7a73b6",
+    "url": "/new-react-portfolio/static/media/profilepic.a00bdf99.png"
+  }
+]);
